668192a8bcf345d612a12cdfd935d92a06c3f9f8
Jonathan Wong <jonathanwong592@gmail.com>
2020-05-08 14:44:08 -0400

    Added project section

--- script.js
+++ script.js
@@ -12,8 +12,25 @@
 //   }
 // });
 
+const innerCard = document.querySelectorAll('.inner-card')
+const innerContent = document.querySelectorAll('.inner-content')
 
- // Option 3 - Smooth Scroll - https://github.com/cferdinandi/smooth-scroll
- const scroll = new SmoothScroll('.navbar a[href*="#"]', {
+for(var i = 0; i < innerCard.length; i++){
+	innerCard[i].addEventListener('mouseover', function() {
+		for(var i = 0; i < innerContent.length; i++){
+			innerContent[i].className = 'inner-content'
+		}
+		document.getElementById(this.dataset.id).className = 'inner-content active'
+		
+		for(var i = 0; i < innerCard.length; i++){
+			innerCard[i].className = 'inner-card'
+		}
+		this.className = 'inner-card active'
+	})
+}
+
+
+// Option 3 - Smooth Scroll - https://github.com/cferdinandi/smooth-scroll
+const scroll = new SmoothScroll('.navbar a[href*="#"]', {
 	speed: 700
 });
